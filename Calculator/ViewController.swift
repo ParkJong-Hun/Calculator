97258321187fd9a68b49e755af07c8909defde74
ParkJong-Hun <thesn515@gmail.com>
2021-08-18 21:03:12 +0900

    2021/08/18 21:02 Fix 0 no delete

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inputNumber.addTarget(self, action: #selector(self.changed_number_to_edit(_:)), for: .editingChanged)
+        self.inputNumber.addTarget(self, action: #selector(self.changed_number_to_edit(_:)), for: .valueChanged)
         inputNumber.text = "0"
     }
     
@@ -34,51 +36,70 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func changed_number_to_edit(_ sender: Any) {
+        if inputNumber.text!.count > 1 {
+            let firstIndex = inputNumber.text!.startIndex
+            if inputNumber.text![firstIndex] == "0" {
+                inputNumber.text!.removeFirst()
+            } else if inputNumber.text![firstIndex] == "-" {
+                let secondIndex = inputNumber.text?.index(after: firstIndex)
+                if inputNumber.text![secondIndex!] == "0" {
+                    inputNumber.text!.remove(at: secondIndex!)
+                }
+            }
+        }
+    }
     
     
     
     //MARK: Digit
     //0
     @IBAction func clicked_0(_ sender: Any) {
-        inputNumber.text! += "0"
+        button_clicked("0")
     }
     //1
     @IBAction func clicked_1(_ sender: Any) {
-        inputNumber.text! += "1"
+        button_clicked("1")
     }
     //2
     @IBAction func clicked_2(_ sender: Any) {
-        inputNumber.text! += "2"
+        button_clicked("2")
     }
     //3
     @IBAction func clicked_3(_ sender: Any) {
-        inputNumber.text! += "3"
+        button_clicked("3")
     }
     //4
     @IBAction func clicked_4(_ sender: Any) {
-        inputNumber.text! += "4"
+        button_clicked("4")
     }
     //5
     @IBAction func clicked_5(_ sender: Any) {
-        inputNumber.text! += "5"
+        button_clicked("5")
     }
     //6
     @IBAction func clicked_6(_ sender: Any) {
-        inputNumber.text! += "6"
+        button_clicked("6")
     }
     //7
     @IBAction func clicked_7(_ sender: Any) {
-        inputNumber.text! += "7"
+        button_clicked("7")
     }
     //8
     @IBAction func clicked_8(_ sender: Any) {
-        inputNumber.text! += "8"
+        button_clicked("8")
     }
     //9
     @IBAction func clicked_9(_ sender: Any) {
-        inputNumber.text! += "9"
+        button_clicked("9")
     }
     
+    func button_clicked(_ num:String) {
+        inputNumber.text! += num
+        if inputNumber.text! == "0\(num)" {
+            inputNumber.text!.removeFirst()
+        }
+    }
     
     
     
