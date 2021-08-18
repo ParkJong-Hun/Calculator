94e2ebcd9367ed1c1f67e4f9cb25772e286bc6c8
ParkJong-Hun <thesn515@gmail.com>
2021-08-18 17:01:00 +0900

    2021/08/18 17:00 Digit, Dot, Toggle, Backspace

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,108 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        inputNumber.text = "0"
     }
-
-
+    
+    
+    
+    
+    
+    //MARK: Input Number
+    @IBOutlet weak var inputNumber: UITextField!
+    
+    @IBAction func changed_number(_ sender: UITextField) {
+        if inputNumber.text!.count > 1 {
+            let firstIndex = inputNumber.text!.startIndex
+            if inputNumber.text![firstIndex] == "0" {
+                inputNumber.text!.removeFirst()
+            } else if inputNumber.text![firstIndex] == "-" {
+                let secondIndex = inputNumber.text?.index(after: firstIndex)
+                if inputNumber.text![secondIndex!] == "0" {
+                    inputNumber.text!.remove(at: secondIndex!)
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    //MARK: Digit
+    //0
+    @IBAction func clicked_0(_ sender: Any) {
+        inputNumber.text! += "0"
+    }
+    //1
+    @IBAction func clicked_1(_ sender: Any) {
+        inputNumber.text! += "1"
+    }
+    //2
+    @IBAction func clicked_2(_ sender: Any) {
+        inputNumber.text! += "2"
+    }
+    //3
+    @IBAction func clicked_3(_ sender: Any) {
+        inputNumber.text! += "3"
+    }
+    //4
+    @IBAction func clicked_4(_ sender: Any) {
+        inputNumber.text! += "4"
+    }
+    //5
+    @IBAction func clicked_5(_ sender: Any) {
+        inputNumber.text! += "5"
+    }
+    //6
+    @IBAction func clicked_6(_ sender: Any) {
+        inputNumber.text! += "6"
+    }
+    //7
+    @IBAction func clicked_7(_ sender: Any) {
+        inputNumber.text! += "7"
+    }
+    //8
+    @IBAction func clicked_8(_ sender: Any) {
+        inputNumber.text! += "8"
+    }
+    //9
+    @IBAction func clicked_9(_ sender: Any) {
+        inputNumber.text! += "9"
+    }
+    
+    
+    
+    
+    
+    //MARK: Dot, Toggle
+    //.
+    @IBAction func clicked_dot(_ sender: Any) {
+        inputNumber.text! += "."
+    }
+    //+/-
+    @IBAction func clicked_toggle(_ sender: Any) {
+        let firstIndex = inputNumber.text?.startIndex
+        if inputNumber.text![firstIndex!] == "-" {
+            inputNumber.text?.removeFirst()
+        } else {
+            if inputNumber.text! != "0" {
+                inputNumber.text?.insert("-", at: firstIndex!)
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    //MARK: Backspace
+    //↩︎
+    @IBAction func clicked_backspace(_ sender: Any) {
+        inputNumber.text?.removeLast()
+        if inputNumber.text?.count == 0 {
+            inputNumber.text! += "0"
+        }
+    }
 }
-
